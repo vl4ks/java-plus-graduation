fda86ee8921bdd3be06559891cb6cec657e802d7
vl4ks <veli4koksenia@yandex.ru>
2025-06-17 14:11:40 +0500

    Шаг 3. Настройка Spring Cloud Config Server

--- infra/config-server/src/main/java/ru/practicum/ConfigServerApplication.java
+++ infra/config-server/src/main/java/ru/practicum/ConfigServerApplication.java
@@ -1,7 +1,13 @@
 package ru.practicum;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigServerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
