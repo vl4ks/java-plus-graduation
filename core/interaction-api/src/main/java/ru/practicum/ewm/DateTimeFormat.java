8cddc3497672eb3d4e96446e0faab6e811ae6b1a
vl4ks <veli4koksenia@yandex.ru>
2025-07-15 20:49:09 +0500

    remove unused string

--- core/interaction-api/src/main/java/ru/practicum/ewm/DateTimeFormat.java
+++ core/interaction-api/src/main/java/ru/practicum/ewm/DateTimeFormat.java
@@ -1,8 +1,5 @@
 package ru.practicum.ewm;
 
-import java.time.format.DateTimeFormatter;
-
 public class DateTimeFormat {
     public static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
-    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_PATTERN);
 }
